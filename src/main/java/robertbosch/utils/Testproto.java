88bea058a71e1729ada84c2624404707d8059f2f
Sahil Tyagi <sahiltyagi@Sahils-MacBook.local>
2018-01-17 14:04:49 +0530

    reading proto data

--- src/main/java/robertbosch/utils/Testproto.java
+++ src/main/java/robertbosch/utils/Testproto.java
@@ -1,14 +1,40 @@
 package robertbosch.utils;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.google.protobuf.util.JsonFormat;
 import com.protoTest.smartcity.Actuated;
 
 public class Testproto {
 	
 	public static void main(String[] args) {
-		writeProtodata();
+		//writeProtodata();
+		readProtoData();
+	}
+	
+	private static void readProtoData() {
+		
+		try {
+			Actuated.targetConfigurations confs = Actuated.targetConfigurations.parseFrom(new FileInputStream("/Users/sahiltyagi/Desktop/out.txt"));
+			System.out.println(confs.getPowerState().getTargetPowerState());
+			System.out.println(confs.getControlPolicy().getControlPolicy());
+			System.out.println(confs.getManualControlParams().getTargetBrightnessLevel());
+			
+			
+			Object ob = Actuated.targetConfigurations.parseFrom(new FileInputStream("/Users/sahiltyagi/Desktop/out.txt"));
+			String packet=JsonFormat.printer().print((Actuated.targetConfigurations)ob);
+			System.out.println(packet);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void readInRemoteMode() {
+		
+		
 	}
 	
 	private static void writeProtodata() {
