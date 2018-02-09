dc80b8c9d0555c80817ecf407adf9e780e116a5e
Sahil Tyagi <sahiltyagi@Sahils-MacBook.local>
2018-02-09 23:17:28 +0530

    testing rabbitMQ latency inside the IISc network

--- src/main/java/robertbosch/middleware/benchmarking/latencycheck.java
+++ src/main/java/robertbosch/middleware/benchmarking/latencycheck.java
@@ -17,6 +17,8 @@ public class latencycheck {
 		String sub = "/home/etl_subsystem/subscribe.txt";
 		String lat = "/home/etl_subsystem/latency_out.txt";
 		String pub = "/home/etl_subsystem/publish.txt";
+		int ctr=0;
+		long sumlatency=0L;
 		
 		BufferedReader subrdr = new BufferedReader(new InputStreamReader(new FileInputStream(sub)));
 		BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(lat)));
@@ -27,12 +29,18 @@ public class latencycheck {
 			while((line=pubrdr.readLine()) !=null) {
 				if(rec.split(",")[1].equalsIgnoreCase(line.split(",")[1])) {
 					System.out.println(rec.split(",")[1] + "    " + line.split(",")[1]);
+					long val = (Long.parseLong(rec.split(",")[0]) - Long.parseLong(line.split(",")[0]));
+					if(val>0) {
+						sumlatency +=val;
+						ctr++;
+					}
 					output.write(String.valueOf(Long.parseLong(rec.split(",")[0]) - Long.parseLong(line.split(",")[0])) + "\n");
 				}
 			}
 			pubrdr.close();
 		}
 		
+		System.out.println("total latency incurred: " + Double.parseDouble(String.valueOf(sumlatency/ctr)));
 		subrdr.close();
 		output.close();
 	}
